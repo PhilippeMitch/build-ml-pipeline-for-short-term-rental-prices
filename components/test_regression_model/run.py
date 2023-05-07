78edcf7cefafb8d855e873219bbcd12054cfb0b2
PhilippeMitch <philippemitch91@gmail.com>
2023-05-07 17:47:54 +0700

    Update the script for the test

--- components/test_regression_model/run.py
+++ components/test_regression_model/run.py
@@ -7,7 +7,7 @@ import logging
 import wandb
 import mlflow
 import pandas as pd
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 from wandb_utils.log_artifact import log_artifact
 
@@ -28,26 +28,28 @@ def go(args):
 
     # Download test dataset
     test_dataset_path = run.use_artifact(args.test_dataset).file()
-
+    logger.info(f"Download test dataset {test_dataset_path}")
     # Read test dataset
     X_test = pd.read_csv(test_dataset_path)
     y_test = X_test.pop("price")
-
-    logger.info("Loading model and performing inference on test set")
+    logger.info(f"Loading model and performing inference on test set")
     sk_pipe = mlflow.sklearn.load_model(model_local_path)
     y_pred = sk_pipe.predict(X_test)
 
     logger.info("Scoring")
     r_squared = sk_pipe.score(X_test, y_test)
 
-    mae = mean_absolute_error(y_test, y_pred)
+    #mae = mean_absolute_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
 
     logger.info(f"Score: {r_squared}")
-    logger.info(f"MAE: {mae}")
+    #logger.info(f"MAE: {mae}")
+    logger.info(f"MSE: {mse}")
 
     # Log MAE and r2
     run.summary['r2'] = r_squared
-    run.summary['mae'] = mae
+    #run.summary['mae'] = mae
+    run.summary['mse'] = mse
 
 
 if __name__ == "__main__":
